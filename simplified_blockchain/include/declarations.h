cb1e0b8623dc8cfd9cfebcdc4c32555aae5f2342
simces <cesnaviciuss@gmail.com>
2024-01-02 18:18:13 +0200

    2021 versija fixed up

---
+++ simplified_blockchain/include/declarations.h
@@ -0,0 +1,20 @@
+#ifndef DELCARATIONS_H
+#define DELCARATIONS_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <bitset>
+#include <math.h>
+#include <ctime>
+#include <vector>
+#include <time.h>
+#include <chrono>
+using namespace std;
+
+std::string hashFunction(std::string hashInput);
+
+#endif
